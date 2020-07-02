834966bb167333224c30fa314bdbd5475a6d0fcb
Erikqtx <glaser-erik@web.de>
2020-07-02 15:46:09 +0200

    Adding Basic Map Functions

--- studienarbeit/src/App.js
+++ studienarbeit/src/App.js
@@ -1,14 +1,20 @@
 import React from "react";
+import ReactDOM from "react-dom";
 import "./css/App.css";
+import "./Map/styles.css";
+import MapChart from "./Map/MapChart";
 function App() {
   return (
     <div className="App">
       <h1 className="header">
         Police Killings in America
         <hr />
+        <MapChart />
       </h1>
     </div>
   );
 }
 
 export default App;
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
