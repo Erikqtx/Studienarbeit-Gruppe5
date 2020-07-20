5a4314fee614a0d12dfa56bf3f31139a5fae4a57
elissa26 <elissa.rimovecz@gmail.com>
2020-07-20 15:48:37 +0200

    function of buttons

--- studienarbeit/src/Buttons/Button.js
+++ studienarbeit/src/Buttons/Button.js
@@ -1,21 +1,36 @@
 import React from "react";
 import "../css/Button.css";
 
+
 function Button() {
+  
+   function Country(){
+     console.log("Country")
+   }
+
+   function State(){
+    console.log("State")
+  }
+
+  function year2015(){
+    console.log("chartof2015")
+  }
+
+  function year2016(){
+    console.log("chartof2016")
+  }
+
   return (
     <div>
-      <div class="button-box1" id="cs">
-        <input type="radio" name="us" id="country" checked hidden />
-        <label for="country">Country</label>
-        <input type="radio" name="us" id="state" hidden />
-        <label for="state">State</label>
+      <div class="button-box1">
+        <button class ="country" onClick = {Country}>Country</button>
+        <button class ="state" onClick = {State}>State</button>
       </div>
 
-      <div class="button-box2">
-        <input type="radio" name="year" id="year1" checked hidden />
-        <label for="year1">2015</label>
-        <input type="radio" name="year" id="year2" hidden />
-        <label for="year2">2016</label>
+
+      <div class = "button-box2">
+        <button class ="year1" onClick = {year2015}>2015</button>
+        <button class ="year2" onClick = {year2016}>2016</button>
       </div>
     </div>
   );
