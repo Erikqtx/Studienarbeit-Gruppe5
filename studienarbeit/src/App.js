4eeeff9dfeceae0219661acb2ab11c06e72f25f3
moritzneubauer <moritzn97@web.de>
2020-08-01 23:09:48 +0200

    Adding Text

--- studienarbeit/src/App.js
+++ studienarbeit/src/App.js
@@ -17,7 +17,9 @@ function App() {
         </h1>
       </div>
       <div class="box text">
-        <p>Hallo ich bin ein Text</p>
+        <p>In Germany, 10 People died by the hand of the Police in 2015, in 2016 there where 11. Compared to the population, this results in a balance of 1.3 per 10.000.000 inhabitants. 
+          <br></br>
+          For current reasons, the composition of the almost 50-times-bigger balance of the USA is shown here.</p>
       </div>
 
       <div className="box map">
@@ -25,7 +27,7 @@ function App() {
         <MapChart />
       </div>
       <div className="box info">
-        <p>Info</p>
+        <h2>Info</h2>
         <PoliceKillings2015Provider>
           <USAChart2015></USAChart2015>
         </PoliceKillings2015Provider>
