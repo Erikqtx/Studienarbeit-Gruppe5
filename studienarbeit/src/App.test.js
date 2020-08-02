4b5ca2e6256d7ecf59135f7bd4f6cd3b575345c9
moritzneubauer <moritzn97@web.de>
2020-08-02 13:59:35 +0200

    Testing Snapshots & Rendering

--- studienarbeit/src/App.test.js
+++ studienarbeit/src/App.test.js
@@ -1,9 +1,21 @@
 import React from "react";
+import ReactDOM from 'react-dom';
 import { render } from "@testing-library/react";
 import App from "./App";
+import renderer from 'react-test-renderer';
 
 test("renders Police Killings Header", () => {
   // const { getByText } = render(<App />);
   // const linkElement = getByText(/learn react/i);
   // expect(linkElement).toBeInTheDocument();
 });
+
+it("renders without crashing", () => {
+  const div = document.createElement("div");
+  ReactDOM.render(<App/>, div)
+})
+
+it("matches the snapshot", () => {
+  const tree = renderer.create(<App/>).toJSON();
+  expect(tree).toMatchSnapshot();
+})
--- studienarbeit/src/Map/MapData/MapChart.test.js
+++ studienarbeit/src/Map/MapData/MapChart.test.js
@@ -2,8 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import MapChart from "./../MapChart";
 import "@testing-library/jest-dom";
+import renderer from 'react-test-renderer';
 
 it("renders without crashing", () => {
     const div = document.createElement("div");
     ReactDOM.render(<MapChart/>, div)
+})
+
+it("matches the snapshot", () => {
+    const tree = renderer.create(<MapChart/>).toJSON();
+    expect(tree).toMatchSnapshot();
 })
