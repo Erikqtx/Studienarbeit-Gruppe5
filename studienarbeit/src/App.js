b2e2855a1e151dc245ab0270a8e45a9278d14b22
moritzneubauer <moritzn97@web.de>
2020-08-02 16:36:42 +0200

    Testing App RenderinTesting App Renderingg

--- studienarbeit/src/App.js
+++ studienarbeit/src/App.js
@@ -7,7 +7,7 @@ import Dummy from "./dummy";
 import PoliceKillings2015Provider from "./police-killings-context/police-killings-context-2015";
 import Main from "./main";
 function App() {
-  
+
   return (
     <div className="App">
       <div className="box header">
@@ -18,7 +18,7 @@ function App() {
       </div>
       <div className="box text">
         <p>In Germany, 10 People died by the hand of the Police in 2015, in 2016 there where 11. Compared to the population, this results in a balance of 1.3 per 10.000.000 inhabitants. 
-          <br></br>
+          <br/>
           For current reasons, the composition of the almost 50-times-bigger balance of the USA is shown here.</p>
       </div>
 
@@ -29,7 +29,7 @@ function App() {
       <div className="box info">
         <h2>Info</h2>
         <PoliceKillings2015Provider>
-          <USAChart2015></USAChart2015>
+          <USAChart2015/>
         </PoliceKillings2015Provider>
       </div>
     </div>
--- studienarbeit/src/App.test.js
+++ studienarbeit/src/App.test.js
@@ -2,7 +2,11 @@ import React from "react";
 import ReactDOM from 'react-dom';
 import { render } from "@testing-library/react";
 import App from "./App";
+import Button from "./Buttons/Button";
+import USAChart from "./charts/USAChart";
+import { PoliceKillings2015Provider } from "./police-killings-context/police-killings-context-2015";
 import renderer from 'react-test-renderer';
+import MapChart from "./Map/MapChart";
 
 it("renders without crashing", () => {
   const div = document.createElement("div");
@@ -17,4 +21,19 @@ it("matches the snapshot", () => {
 it("renders header", () => {
     const {container} = render(<h1/>)
     container.querySelector('box header')
-  });
+});
+
+it("renders text", () => {
+     const {container} = render(<p/>)
+     container.querySelector('box text')
+});
+
+it("renders box map", () => {
+     const {container} = render(<Button/>, <MapChart/>)
+     container.querySelector('box map')
+});
+
+it("renders box info", () => {
+   const {container} = render(<h2/>, <PoliceKillings2015Provider/>, <USAChart2015/>)
+   container.querySelector('box info')
+});
