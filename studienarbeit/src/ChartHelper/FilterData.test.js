07b286c64d5a6722f87abff65a51b2efdb4b22c5
Erikqtx <glaser-erik@web.de>
2020-08-04 00:13:39 +0200

    Tests for FilterData

---
+++ studienarbeit/src/ChartHelper/FilterData.test.js
@@ -0,0 +1,55 @@
+import { extractMonths, extractRace } from "./FilterData";
+describe("charthelper", () => {
+  describe("extractMonths", () => {
+    it("should return an empty list on no input", () => {
+      expect(extractMonths([])).toEqual([]);
+    });
+    it("should extract months as a string", () => {
+      const input = [
+        {
+          uid: "2",
+          name: "Matthew Ajibade",
+          age: "22",
+          gender: "Male",
+          raceethnicity: "Black",
+          month: "January",
+          day: "1",
+          year: "2015",
+          streetadress: "1050 Carl Griffin Dr",
+          city: "Savannah",
+          state: "GA",
+          classification: "Death in custody",
+          lawenforcementagency: "Chatham County Sheriff's Office",
+          armed: "No",
+        },
+      ];
+      expect(extractMonths(input)).toEqual([input[0].month]);
+    });
+  });
+  describe("extractRace", () => {
+    it("should return an empty list on no input", () => {
+      expect(extractRace([])).toEqual([]);
+    });
+    it("should extract the race as a string", () => {
+      const input = [
+        {
+          uid: "2",
+          name: "Matthew Ajibade",
+          age: "22",
+          gender: "Male",
+          raceethnicity: "Black",
+          month: "January",
+          day: "1",
+          year: "2015",
+          streetadress: "1050 Carl Griffin Dr",
+          city: "Savannah",
+          state: "GA",
+          classification: "Death in custody",
+          lawenforcementagency: "Chatham County Sheriff's Office",
+          armed: "No",
+        },
+      ];
+      expect(extractRace(input)).toEqual([input[0].raceethnicity]);
+    });
+  });
+});
