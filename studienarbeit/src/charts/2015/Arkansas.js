d68e30b8d6f4e063c55ae25f885f5c5d4730bfb8
Erikqtx <glaser-erik@web.de>
2020-08-04 02:49:52 +0200

    State Graphs Part 1

---
+++ studienarbeit/src/charts/2015/Arkansas.js
@@ -0,0 +1,51 @@
+import React, { useContext, useMemo } from "react";
+import { Bar } from "react-chartjs-2";
+import { PoliceKillings2015Context } from "../../../police-killings-context/police-killings-context-2015";
+import { extractState } from "../../../ChartHelper/FilterData";
+export default function ArkansasChart2015() {
+  //load our data and save it
+  const [killings] = useContext(PoliceKillings2015Context);
+  //use our filter function
+  const state = extractState(killings);
+  //To get January entries
+  const arkansas = state.filter((datapoint) => datapoint === "AR");
+  const arkansasl = arkansas.length;
+
+  const data = useMemo(() => ({
+    options: {
+      scales: {
+        yAxes: [
+          {
+            ticks: {
+              min: 0,
+            },
+          },
+        ],
+      },
+    },
+    labels: ["Arkansas"],
+    datasets: [
+      {
+        label: "Deaths per Year",
+        backgroundColor: "rgba(255,99,132,0.2)",
+        borderColor: "rgba(255,99,132,1)",
+        borderWidth: 2,
+        hoverBackgroundColor: "rgba(255,99,132,0.4)",
+        hoverBorderColor: "rgba(255,99,132,1)",
+
+        data: [arkansasl],
+      },
+    ],
+  }));
+
+  return (
+    <Bar
+      data={data}
+      width="300"
+      height="200"
+      options={{
+        maintainAspectRatio: true,
+      }}
+    />
+  );
+}
---
+++ studienarbeit/src/charts/2016/Delaware.js
@@ -0,0 +1,51 @@
+import React, { useContext, useMemo } from "react";
+import { Bar } from "react-chartjs-2";
+import { PoliceKillings2016Context } from "../../../police-killings-context/police-killings-context-2016";
+import { extractState } from "../../../ChartHelper/FilterData";
+export default function DelawareChart2016() {
+  //load our data and save it
+  const [killings] = useContext(PoliceKillings2016Context);
+  //use our filter function
+  const state = extractState(killings);
+  //To get January entries
+  const delaware = state.filter((datapoint) => datapoint === "DE");
+  const delawarel = delaware.length;
+
+  const data = useMemo(() => ({
+    options: {
+      scales: {
+        yAxes: [
+          {
+            ticks: {
+              min: 0,
+            },
+          },
+        ],
+      },
+    },
+    labels: ["Delaware"],
+    datasets: [
+      {
+        label: "Deaths per Year",
+        backgroundColor: "rgba(255,99,132,0.2)",
+        borderColor: "rgba(255,99,132,1)",
+        borderWidth: 2,
+        hoverBackgroundColor: "rgba(255,99,132,0.4)",
+        hoverBorderColor: "rgba(255,99,132,1)",
+
+        data: [delawarel],
+      },
+    ],
+  }));
+
+  return (
+    <Bar
+      data={data}
+      width="300"
+      height="200"
+      options={{
+        maintainAspectRatio: true,
+      }}
+    />
+  );
+}
